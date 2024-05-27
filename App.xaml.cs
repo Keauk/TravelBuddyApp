1d2bf026414e91072d2d772f860ed4ea274f95b5
Keauk <keauk8@gmail.com>
2024-05-27 12:31:29 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TravelBuddyApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}

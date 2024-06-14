fee74a253100954986f38a5161b417bec507dcd2
Keauk <keauk8@gmail.com>
2024-06-14 22:16:01 +0200

    Remove unused imports in TripResponse model.

--- Models/TripResponse.cs
+++ Models/TripResponse.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TravelBuddyApp.Models
+﻿namespace TravelBuddyApp.Models
 {
     public class TripResponse
     {
